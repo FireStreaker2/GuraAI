6feb54a0f0fa8f2bd2d7d1a2330ee02b2160ebb9
FireStreaker2 <michaelccc2019@outlook.com>
2023-11-18 10:36:55 -0800

    setup files

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
